39d807844c082f4cb96871aac42cc7e8c6010dee
Radhika kapoor <radhikakapoor383@gmail.com>
2026-03-21 01:10:18 +0500

    new file= UserProfilr created in which data is fetched from the api, and that compoment is called at main.jsx

---
+++ src/UserProfile.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+
+function UserProfile() {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users/1")
+            .then(response => response.json())
+            .then(data => {
+                setUser(data);
+                setLoading(false);
+            })
+            .catch(err => {
+                setError("Failed to fetch user data");
+                setLoading(false);
+            });
+    }, []);
+    if (loading) {
+        return(
+            <p>Loading... ⏳</p>
+        );
+    }
+    if (error !== "") {
+        return (
+            <>
+                <p style={{ color: "red" }}>{error}</p>);
+                <button onClick={() => window.location.reload()}>Reload 🔄</button>
+            </>
+            );
+    }
+    if (user) {
+        return (
+            <div style={{ border: "1px solid black", padding: "10px", maxWidth: "400px" }}>
+                <h2>{user.name}</h2>
+                <p><strong>Email:</strong> {user.email}</p>
+                <p><strong>Phone:</strong> {user.phone}</p>
+                <p><strong>Company:</strong> {user.company.name}</p>
+                <p><strong>Address:</strong> {user.address.street}, {user.address.city}</p>
+                <button onClick={() => window.location.reload()}>Reload 🔄</button>
+            </div>
+        );
+    }
+    return null;
+}
+export default UserProfile;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import App from './App.jsx';
 import ATM from './ATM-machine.jsx';
 import {ShoppingCart} from './Shopping-cart.jsx';
 import LiveDashboard from './live-dashboard.jsx';
+import UserProfile from './UserProfile.jsx';
 
 createRoot(document.getElementById('root')).render(
   <div>
     {/* <App /> */}
     {/* <ATM /> */}
     {/* <ShoppingCart /> */}
-    <LiveDashboard stdname="Radhika" />
+    {/* <LiveDashboard stdname="Radhika" /> */}
+    <UserProfile />
+
   </div>
 )
