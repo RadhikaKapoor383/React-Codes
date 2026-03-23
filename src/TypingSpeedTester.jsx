3d27a71b93969294fc5303d213ca17bd0a110f27
Radhika kapoor <radhikakapoor383@gmail.com>
2026-03-24 02:46:28 +0500

    Halfway to the logic

---
+++ src/TypingSpeedTester.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+function TypingTest() {
+    const [targetText] = "The quick brown fox.";
+    const [typed, setTyped] = useState("");
+    const [timer, setTimer] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    const [finished, setFinished] = useState(false);
+
+    useEffect(() => {
+        if (!isRunning) return;
+
+        const interval = setInterval(() => {
+            setTimer(prev => prev + 1);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [isRunning]);
+    function handleTyping (e) {
+        const value = e.target.value;
+        setTyped(value);
+
+        if(value.length === 1) {
+            setIsRunning(true);
+        }
+        if(typed === targetText) {
+            setIsRunning(false);
+            setFinished(true);
+        }
+    }
+    function Calculate() {
+        
+    }
+}
