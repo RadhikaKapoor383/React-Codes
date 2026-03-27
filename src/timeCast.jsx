b4ec70cb201b1738e4e253e9c5cb1a67b0212d49
Radhika kapoor <radhikakapoor383@gmail.com>
2026-03-28 00:26:12 +0500

    edit the file timeCast just for fun

--- src/timeCast.jsx
+++ src/timeCast.jsx
@@ -1,1 +1,10 @@
-import { useState } from "react";
+export default function App() {
+    const [text, setText] = useState("");
+    return (
+        <div>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
+            <p>Hello</p>
+        </div>
+        
+  );
+}
