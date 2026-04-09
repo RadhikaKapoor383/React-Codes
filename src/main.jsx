350f86d4b876ff423527c31d8ff481dddf3757c7
Radhika kapoor <radhikakapoor383@gmail.com>
2026-04-10 01:19:28 +0500

    remove unnecessary imports from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import TrafficLight from './Traffic-LightApp.jsx';
-import TipCalculator from './TipCalculator.jsx';
-import App from './FeedbackForm.jsx'
-import TypingTest from './TypingSpeedTester.jsx';
-import PasswordChecker from './PasswordChecker.jsx';
 import WeatherCard from './WeatherCard.jsx';
 
 
