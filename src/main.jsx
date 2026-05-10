091ec9c6e41006ee436db45d9e8d934211a0d2a2
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-11 01:03:10 +0500

    import userCard and add that component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { createRoot } from 'react-dom/client';
 import WeatherCard from './WeatherCard.jsx';
 import Trafficlight from './Traffic-LightApp.jsx';
 import UniversityApplicationForm from './UniversityApplicationForm.jsx';
+import UserCard from './life-cycle/user-data-fetcher.jsx';
 
 createRoot(document.getElementById('root')).render(
   <div>
-    <UniversityApplicationForm />
+    <UserCard />
   </div>
 );
