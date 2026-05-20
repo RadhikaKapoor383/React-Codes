f74a0dabe7b45f6c9344f91a1ab33147cfb0e3c6
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-20 16:50:45 +0500

    Replace StudentsData component with Counter component for state management demonstration

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import UniversityApplicationForm from './UniversityApplicationForm.jsx';
 import UserCard from './life-cycle/user-data-fetcher.jsx';
 import App from './App.jsx';
 import StudentsData from './Students.jsx';
+import Counter from './useState.jsx';
 
 createRoot(document.getElementById('root')).render(
   <div>
-    <StudentsData />
+    <Counter />
   </div>
 );
---
+++ src/useState.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+export default function Counter() {
+    const [ count, setCount ] = useState(0);
+    const [ isOpen, setIsOpen ] = useState(false);
+    return(
+        <div>
+            <h1>Count: {count}</h1>
+            <h1>Toggle: {isOpen ? "Open" : "Closed"}</h1>
+            <button onClick={() => setCount(prevCount=>prevCount+1)}>Count</button>
+            <button onClick={() => setIsOpen(!isOpen)}>Toggle</button>
+        </div>
+    );
+}
